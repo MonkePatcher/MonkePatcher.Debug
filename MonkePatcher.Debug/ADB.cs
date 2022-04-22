866b09547df7c1f431a8c80653ca496a4cd15c2a
MillzyDev <millzyyt08@gmail.com>
2022-04-22 08:26:13 +0100

    ADB Shell

--- MonkePatcher.Debug/ADB.cs
+++ MonkePatcher.Debug/ADB.cs
@@ -6,5 +6,13 @@ namespace MonkePatcher.Debug
     {
         public static void OpenLogcatWindow()
             => Process.Start($"cmd.exe {Path.Combine(SDKInstaller.SDKBinPath, "adb.exe")} logcat");
+
+        public static StreamReader Shell(params string[] args)
+        { 
+            var process = Process.Start($"cmd.exe {Path.Combine(SDKInstaller.SDKBinPath, "adb.exe")} shell {string.Join(" ", args)}");
+            process.BeginOutputReadLine();
+            var output = process.StandardOutput;
+            return output;
+        }
     }
 }
